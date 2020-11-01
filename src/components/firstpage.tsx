bd027ee854682dcd27a8b9def1b2cb6cbaab82a4
Hassan Arshad <hassanarshad50@yahoo.com>
2020-11-01 15:31:30 -0500

    Added firstpage

--- src/components/firstpage.tsx
+++ src/components/firstpage.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from '../resources/images/logo.svg';
-import { Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
+export const FirstPage: FC = () => {
     return (
         <div className='App'>
             <header className='App-header'>
@@ -11,10 +10,10 @@ function App() {
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                <Link to='/firstpage'> Go Here</Link>
+                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+                    Learn React
+                </a>
             </header>
         </div>
     );
-}
-
-export default App;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App';
+import { FirstPage } from './components';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route path='/firstPage'>
+                    <FirstPage />
+                </Route>
+                <Route exact path='/'>
+                    <App />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
